50653a2fd3029df1d379eb7ed262e8043dfc1a28
bokajw <118814209+bokajw@users.noreply.github.com>
2022-11-24 01:33:55 +0100

    ge

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,7 @@ console.log('hello world');
 
 let name = 'jakob wilmer';
 console.log(name);
+// ~name~ (after "let") cannot be a set keyword, such as "let" and "If".
+// it should be meaningful and effective
+// cannot start with number
+// cannot include multiple names or -
